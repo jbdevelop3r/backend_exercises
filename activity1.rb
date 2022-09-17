972167a94fb5ba52c2d6421fb27fdfead349e5db
jbdevelop3r <justinebanogon14@gmail.com>
2022-09-17 21:46:23 +0800

    exercises

--- activity1.rb
+++ activity1.rb
@@ -3,6 +3,17 @@ puts("ruby start")
 
 h1 = { a: 1, b: 2, c: 3, d:4 }
 
-puts(h1[:a])
+puts h1[:a]
 
-puts("hello batch21 - hello")
+h1[:e] = 5
+
+puts h1
+
+puts "item 1"
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+numbers.each do |n| 
+    puts numbers
+
+end
