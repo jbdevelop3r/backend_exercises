7d0a440c1cd24616cf71a879af759d11d9ae2ed8
jbdevelop3r <justinebanogon14@gmail.com>
2022-09-22 20:47:59 +0800

    done act 4

--- Activity-3/activity3.rb
+++ Activity-3/activity3.rb
@@ -1,13 +1,26 @@
 puts "Enter a number"
-num = gets.chomp.to_i
 
-def check_square(number)
+input_number = gets.to_i
+
+# def check_number(number)
+#     for x in (0..number) do
+#         if (x * x) == number 
+#             return puts "#{number} if a perfect square"
+#         elsif (x * x) > number 
+#          puts "#{number} is not a perfect square"
+#         end
+#     end
+# end
+# check_number(input_number)
+
+
+
+def check_number(number)
     for x in (0..number) do
         if (x * x) == number 
-            return puts "#{number} is a perfect square"
-        elsif (x * x) > number
-            return puts "#{number} is not a perfect square"
+            return puts "#{number} if a perfect square" 
         end
     end
+        puts "#{number} is not a perfect square"
 end
-check_square(num)
+check_number(input_number)
