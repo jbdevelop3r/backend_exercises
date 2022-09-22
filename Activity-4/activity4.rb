75f32f5ae2e2d76f64c30670ad1a653eff01dcd3
jbdevelop3r <justinebanogon14@gmail.com>
2022-09-22 20:49:45 +0800

    done act 4

---
+++ Activity-4/activity4.rb
@@ -0,0 +1,16 @@
+class Student
+
+    attr_accessor :s_name, :s_age, :s_address
+    
+    def initialize(name, age, address)
+        @s_name = name
+        @s_age = age
+        @s_address = address
+    end
+end
+
+info = Student.new("Cute", 16, "123 street")
+
+info.s_age = 50 #setter example
+
+puts info.s_age
