ab6deebab1694e83d2349cf2798a82838615c054
jbdevelop3r <justinebanogon14@gmail.com>
2022-10-01 11:59:24 +0800

    done

--- Activity-9-Isogram/isogram.rb
+++ Activity-9-Isogram/isogram.rb
@@ -1,17 +1,31 @@
+# def isogram(word)
+#     sample_word = word.downcase.split('')
+#     sample_word.each_with_index do |element, idx| 
+#         i = idx + 1
+#         while i < sample_word.length  
+#             return false if element == sample_word[i] 
+#             i += 1 
+#             end   
+#         end
+#         true
+# end
+# p isogram("Dermatoglyphics")
+# p isogram("aba")
+# p isogram("moOse")
+# p isogram('abc')
+
 def isogram(word)
-    sample_word = word.downcase.split('')
-    sample_word.each_with_index do |element, idx| 
-        i = idx + 1
-        while i < sample_word.length  
-            return false if element == sample_word[i] 
-            i += 1 
+    checked = []
+    sample_word = word.downcase
+        sample_word.each_char do |char|
+            return false if checked.include? char
+            # checked << char
             end
+            true
         end
-        true
-end
-p isogram("Dermatoglyphics")
-p isogram("aba")
-p isogram("moOse")
-p isogram('abc')
-p isogram('eleven')
+
+puts isogram('justinee')
+
+    
+
 
---
+++ Activity-SQL/activity2-sql.sql
@@ -0,0 +1,34 @@
+CREATE TABLE notes (
+	id int PRIMARY KEY,
+	body VARCHAR(255),
+	student_id int,
+	FOREIGN KEY (student_id) REFERENCES students(id)
+);
+
+SELECT * FROM notes;
+
+INSERT INTO notes (id, body, student_id) VALUES 
+	(501, 'what', 1),
+	(502, 'is', 2),
+	(503, 'your', 3),
+	(504, 'favorite', 4),
+	(505, 'lesson', 5),
+	(506, 'for', 5),
+	(507, 'this', 5),
+	(508, 'weekend', 5),
+	(509, 'backend', NULL),
+	(510, 'discussion', NULL);
+
+SELECT *
+FROM students 
+LEFT JOIN notes
+ON notes.id = student_id;
+
+SELECT *
+FROM students 
+RIGHT JOIN notes
+ON notes.id = student_id;
+
+SELECT * 
+FROM students
+FULL OUTER JOIN notes on students.id = notes.student_id;
